5047aaa6ec23a3523c48d08f5580e4c81a893712
Jacek Jackowiak <jacek1986@gmail.com>
2015-05-17 11:05:08 +0200

    [Game1] Extracting methods

--- src/main/java/pl/airborn/tennis/TennisGame1.java
+++ src/main/java/pl/airborn/tennis/TennisGame1.java
@@ -17,57 +17,64 @@ public class TennisGame1 implements TennisGame {
     }
 
     public String getScore() {
+        if (player1Score == player2Score) {
+            return getEvenScore();
+        } else if (player1Score >= 4 || player2Score >= 4) {
+            return getWiningScore();
+        } else {
+            return getIntermediateScore();
+        }
+    }
+
+    private String getEvenScore() {
+        switch (player1Score) {
+            case 0:
+                return "Love-All";
+            case 1:
+                return "Fifteen-All";
+            case 2:
+                return "Thirty-All";
+            default:
+                return "Deuce";
+        }
+    }
+
+    private String getWiningScore() {
+        int minusResult = player1Score - player2Score;
+        if (minusResult == 1) {
+            return "Advantage player1";
+        } else if (minusResult == -1) {
+            return "Advantage player2";
+        } else if (minusResult >= 2) {
+            return "Win for player1";
+        } else {
+            return "Win for player2";
+        }
+    }
+
+    private String getIntermediateScore() {
         String score = "";
         int tempScore;
-        if (player1Score == player2Score) {
-            switch (player1Score) {
+        for (int i = 1; i < 3; i++) {
+            if (i == 1) {
+                tempScore = player1Score;
+            } else {
+                score += "-";
+                tempScore = player2Score;
+            }
+            switch (tempScore) {
                 case 0:
-                    score = "Love-All";
+                    score += "Love";
                     break;
                 case 1:
-                    score = "Fifteen-All";
+                    score += "Fifteen";
                     break;
                 case 2:
-                    score = "Thirty-All";
+                    score += "Thirty";
                     break;
-                default:
-                    score = "Deuce";
+                case 3:
+                    score += "Forty";
                     break;
-
-            }
-        } else if (player1Score >= 4 || player2Score >= 4) {
-            int minusResult = player1Score - player2Score;
-            if (minusResult == 1) {
-                score = "Advantage player1";
-            } else if (minusResult == -1) {
-                score = "Advantage player2";
-            } else if (minusResult >= 2) {
-                score = "Win for player1";
-            } else {
-                score = "Win for player2";
-            }
-        } else {
-            for (int i = 1; i < 3; i++) {
-                if (i == 1) {
-                    tempScore = player1Score;
-                } else {
-                    score += "-";
-                    tempScore = player2Score;
-                }
-                switch (tempScore) {
-                    case 0:
-                        score += "Love";
-                        break;
-                    case 1:
-                        score += "Fifteen";
-                        break;
-                    case 2:
-                        score += "Thirty";
-                        break;
-                    case 3:
-                        score += "Forty";
-                        break;
-                }
             }
         }
         return score;
