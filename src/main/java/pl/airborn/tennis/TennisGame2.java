f8198ab33af78f6e42cd4bc25b5b31bb0a2a48fb
Jacek Jackowiak <jacek1986@gmail.com>
2015-05-22 20:48:10 +0200

    [Game2] Removing unused methods and fields

--- src/main/java/pl/airborn/tennis/TennisGame2.java
+++ src/main/java/pl/airborn/tennis/TennisGame2.java
@@ -6,12 +6,8 @@ public class TennisGame2 implements TennisGame {
 
     public String P1res = "";
     public String P2res = "";
-    private String player1Name;
-    private String player2Name;
 
     public TennisGame2(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
     }
 
     public String getScore() {
@@ -89,22 +85,6 @@ public class TennisGame2 implements TennisGame {
             score = "Win for player2";
         }
         return score;
-    }
-
-    public void SetP1Score(int number) {
-
-        for (int i = 0; i < number; i++) {
-            P1Score();
-        }
-
-    }
-
-    public void SetP2Score(int number) {
-
-        for (int i = 0; i < number; i++) {
-            P2Score();
-        }
-
     }
 
     public void P1Score() {
