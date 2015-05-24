24cf9722264de093eedbc105afdd1f626f4a0b61
Jacek Jackowiak <jacek1986@gmail.com>
2015-05-24 02:04:22 +0200

    [Game3] Moved deuce to separate branch

--- src/main/java/pl/airborn/tennis/TennisGame3.java
+++ src/main/java/pl/airborn/tennis/TennisGame3.java
@@ -18,10 +18,10 @@ public class TennisGame3 implements TennisGame {
             String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
             s = p[p1];
             return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+        } else if (p1 == p2) {
+            return "Deuce";
         } else {
-            if (p1 == p2) {
-                return "Deuce";
-            }
+
             s = p1 > p2 ? p1N : p2N;
             return ((p1 - p2) * (p1 - p2) == 1) ? "Advantage " + s : "Win for " + s;
         }
